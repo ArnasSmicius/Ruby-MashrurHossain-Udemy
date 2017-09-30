8902cee347426823f828167db4b01370e97485e1
ArnasSmicius <arnasmic@gmail.com>
2017-09-30 14:08:34 +0300

    Working with Strings

---
+++ code/ruby_projects/analizer.rb
@@ -0,0 +1,11 @@
+puts "Enter your first name:"
+first_name = gets.chomp
+
+puts "Enter your last name:"
+last_name = gets.chomp
+
+puts "=============================="
+puts "Your first name has #{first_name.length} characters"
+puts "Your last name has #{last_name.length} characters"
+puts "Your first name reversed looks like \"#{first_name.reverse}\""
+puts "Your last name reversed looks like \"#{last_name.reverse}\""
---
+++ code/ruby_projects/playground.rb
@@ -0,0 +1,9 @@
+puts "Hello enter in you first name:"
+
+first_name = gets.chomp
+
+puts "Please enter your last name:"
+
+last_name = gets.chomp
+
+puts "Welcome to the playground, #{first_name} #{last_name}"
